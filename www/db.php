3bb062d7d81fd6353de237e6fadef9c2c04b186a
tionote <48472698+tionote@users.noreply.github.com>
2019-05-20 11:27:53 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id9299562_tugas7","tugas7","id9299562_tugas7") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id4103787_oktioryan0140","billabong45","id4103787_sertifikasi") or die ("could not connect database");
 ?>
